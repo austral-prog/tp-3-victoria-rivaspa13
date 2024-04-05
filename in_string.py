726dfd44743f13cb1331e006804b26423fd9ac00
victoria-rivaspa13 <162721841+victoria-rivaspa13@users.noreply.github.com>
2024-04-05 12:30:07 -0300

    update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,12 @@
 def check_vowels():
-    # Código a implementar utilizando input.
+	name = input().lower()
+	print("a" in name)
+	print("e" in name)
+	print("i" in name)
+	print("o" in name)
+	print("u" in name)
 
+check_vowels()
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
